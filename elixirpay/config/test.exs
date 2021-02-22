2f8b8841fc26557b2b4dd8e8c6dbf2be066f4189
fcsouza <fcsouza18@hotmail.com>
2021-02-22 12:48:30 -0300

    include config and credo

--- elixirpay/config/test.exs
+++ elixirpay/config/test.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # Run `mix help test` for more information.
 config :elixirpay, Elixirpay.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "docker",
   database: "elixirpay_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
