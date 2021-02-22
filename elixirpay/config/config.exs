20bf7508fb4efdbfad32f34d4ccb2012f6cc6bc4
fcsouza <fcsouza18@hotmail.com>
2021-02-22 12:32:41 -0300

    init elixir project

---
+++ elixirpay/config/config.exs
@@ -0,0 +1,31 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :elixirpay,
+  ecto_repos: [Elixirpay.Repo]
+
+# Configures the endpoint
+config :elixirpay, ElixirpayWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "akR0LnclV32RMHe6Fa5kP0g/VTwqS9/BLB/43c9ds9qVHcYDWDQbGNWLXGZOMB/R",
+  render_errors: [view: ElixirpayWeb.ErrorView, accepts: ~w(json), layout: false],
+  pubsub_server: Elixirpay.PubSub,
+  live_view: [signing_salt: "uoJ1N4lT"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
