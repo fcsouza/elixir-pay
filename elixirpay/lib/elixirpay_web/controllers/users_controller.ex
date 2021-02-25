3ee0c3dc927447924b3fa8c9c85db5c308269ed8
fcsouza <fcsouza18@hotmail.com>
2021-02-25 20:00:14 -0300

    user controller

--- elixirpay/lib/elixirpay_web/controllers/users_controller.ex
+++ elixirpay/lib/elixirpay_web/controllers/users_controller.ex
@@ -8,7 +8,7 @@ defmodule Elixirpay.UsersController do
   action_fallback Elixirpay.FallbackController
 
   def create(conn, params) do
-    with {:ok, %User{} = user}  <- Rocketpay.create_user(params) do
+    with {:ok, %User{} = user}  <- Elixirpay.create_user(params) do
       conn
       |> put_status(:created)
       |> render("create.json", %{user: user})
@@ -16,7 +16,7 @@ defmodule Elixirpay.UsersController do
   end
 
   def list(conn, _params) do
-    with users  <- Rocketpay.list_user() do
+    with users  <- Elixirpay.list_user() do
       conn
       |> put_status(:ok)
       |> render("list.json", %{users: users})
