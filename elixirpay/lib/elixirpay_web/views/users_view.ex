66df5c1fc34f16ac121582f912fc89a06a486918
fcsouza <fcsouza18@hotmail.com>
2021-02-27 08:47:39 -0300

    refactor create and user view

--- elixirpay/lib/elixirpay_web/views/users_view.ex
+++ elixirpay/lib/elixirpay_web/views/users_view.ex
@@ -1,14 +1,53 @@
 defmodule ElixirpayWeb.UsersView do
-  alias Elixirpay.User
+  alias Elixirpay.{Account, User}
 
-  def render("create.json", %{user: %User{id: id, name: name, nickname: nickname}}) do
+  def render("create.json", %{
+        user: %User{
+          id: id,
+          name: name,
+          nickname: nickname,
+          account: %Account{balance: balance, id: account_id}
+        }
+      }) do
     %{
       message: "User created",
       user: %{
         id: id,
         name: name,
-        nickname: nickname
+        nickname: nickname,
+        account: %{
+          id: account_id,
+          balance: balance
+        }
       }
+    }
+  end
+
+  def render("list.json", %{users: users}) do
+    %{
+      message: "List users",
+      users:
+        Enum.map(users, fn %{
+                             id: id,
+                             name: name,
+                             nickname: nickname,
+                             age: age,
+                             account: %{
+                               id: account_id,
+                               balance: balance
+                             }
+                           } ->
+          %{
+            id: id,
+            name: name,
+            age: age,
+            nickname: nickname,
+            account: %{
+              balance: balance,
+              id: account_id
+            }
+          }
+        end)
     }
   end
 end
